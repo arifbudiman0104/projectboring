c59cdd467b411fcc17618606508c12a8ba7d8113
Arif Budiman Arrosyid <48200892+arifbudimanarrosyid@users.noreply.github.com>
2021-06-03 21:15:03 +0700

    membuat ui home

---
+++ resources/views/component/footer.blade.php
@@ -0,0 +1,10 @@
+{{-- footer --}}
+<div id="footer">
+    <footer class="bg-light text-center text-lg-start">
+        
+        <div class="text-center p-3 bg-dark text-white">
+            ©2021 Made by Arif Budiman Arrosyid
+        </div>
+    </footer>
+
+</div>
---
+++ resources/views/component/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-md fixed-top navbar-light p-3 bg-light bd-navbar d-flex justify-content-end ">
+    <div class="container-xxl flex-wrap flex-md-wrap">
+        <a class="navbar-brand" href="/">PROJECT BORING</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+            <div class="navbar-nav ">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <a class="nav-link active" aria-current="page" href="/blog">Blog</a>
+                <a class="nav-link active" aria-current="page" href="/portofolio">Portofolio</a>
+                <a class="nav-link active" aria-current="page" href="/about">About</a>
+                <a class="nav-link active" aria-current="page" href="https://github.com/arifbudimanarrosyid">Github</a>
+                
+            </div>
+        </div>
+    </div>
+    </div>
+</nav>
